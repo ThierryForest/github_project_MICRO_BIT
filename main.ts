ab773da30aaa8fc915f449aa7edab70f547d4726
ThierryForest <32687473+ThierryForest@users.noreply.github.com>
2024-11-14 20:44:45 +0100

    Premiers gestes

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+})
+basic.clearScreen()
+basic.showArrow(ArrowNames.North)
+basic.showArrow(ArrowNames.NorthEast)
+basic.showArrow(ArrowNames.East)
+basic.showArrow(ArrowNames.SouthEast)
+basic.clearScreen()
 basic.forever(function () {
     led.plot(2, 0)
 })
